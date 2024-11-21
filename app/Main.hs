0a9c1d314a8d14c177c485dc1401b4398afca90f
FructoseJuice <116052103+FructoseJuice@users.noreply.github.com>
2024-11-20 18:39:57 -0700

    Planets have velocity
    Planets have velocity. Each frame their coordinates are adjusted using this velocity

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,51 @@
 module Main where
-import Graphics.Gloss
 import Planets
+import Graphics.Gloss
+import Graphics.Gloss.Interface.Pure.Game
+import Debug.Trace (trace)
 
-myPlanets = [(Planet 20 red 0 60), (Planet 50 green 0 0), (Planet 30 yellow 0 (-70))]
+-- Initial planets with some velocity
+myPlanets :: [Planet]
+myPlanets = [
+    Planet 20 red 0 60 10 5,
+    Planet 50 green 0 0 6 6,
+    Planet 30 yellow 0 (-70) (-6) 5]
 
 -- Main function to run the program
 main :: IO ()
-main = animate
+main = play
     (InWindow "Planets" (1000, 1000) (100, 100)) -- Window size and position
-    black                                              -- Background color
-    (frame myPlanets)                                            -- Frame function
+    black                                        -- Background color
+    60 -- Frame rate
+    myPlanets -- Initial state
+    renderFrame -- Renders each frame
+    handleEvent -- Event handling, no change to the state in this case
+    updateSimState -- Updates each frame
 
--- Function to generate each frame of the animation
-frame :: [Planet] -> Float -> Picture
-frame ps time = Pictures [ go p | p <- ps ]
+-- Function to render the planets
+renderFrame :: [Planet] -> Picture
+renderFrame = planetsToPicture
+
+
+-- Click event handler to spawn a new planet, Will start with 0 velocity
+handleEvent :: Event -> [Planet] -> [Planet]
+handleEvent (EventKey (MouseButton LeftButton) Down _ (x, y)) ps =
+    (Planet 20 (dark red) x y 0 0) : ps
+
+handleEvent _ ps = ps -- Otherwise, don't change state
+
+
+-- Function to update the planet positions over time
+updateSimState :: Float -> [Planet] -> [Planet]
+updateSimState time ps = map go ps
   where
-    go planet = Translate x (yCoor planet) $ planetToPicture planet
-    x = 100 * sin time -- X-coordinate changes with time
+    -- Should be < 1.0, Higher value yield faster velocities
+    velocityDampeningFactor = 0.07
+
+    go planet = Planet
+       (radius planet)
+       (pColor planet)
+       (xCoor planet + velocityX planet * velocityDampeningFactor)  -- Update x-coordinate based on velocity
+       (yCoor planet + velocityY planet * velocityDampeningFactor)  -- Update y-coordinate based on velocity
+       (velocityX planet) -- Constant velocity
+       (velocityY planet) -- Constant velocity        
--- app/Planets.hs
+++ app/Planets.hs
@@ -1,10 +1,22 @@
 module Planets where
 import Graphics.Gloss
 
-data Planet = Planet { radius :: Float, pColor :: Color, xCoor :: Float, yCoor :: Float }
+data Planet = Planet {
+    radius :: Float,    -- Radius of the planet
+    pColor :: Color,    -- Color of the planet
+    xCoor :: Float,     -- x-coordinate of the planet
+    yCoor :: Float,     -- y-coordinate of the planet
+    velocityX :: Float, -- Horizontal velocity
+    velocityY :: Float  -- Vertical velocity
+}
 
+-- Converts a planet to a picture
 planetToPicture :: Planet -> Picture
-planetToPicture p = Color (pColor p) $ ThickCircle 0 (radius p)
+planetToPicture p = 
+  Translate (xCoor p) (yCoor p) $ 
+  Color (pColor p) $ 
+  ThickCircle 0 (radius p)
 
+-- Converts all planets to pictures
 planetsToPicture :: [Planet] -> Picture
 planetsToPicture ps = Pictures [ planetToPicture p | p <- ps ]
